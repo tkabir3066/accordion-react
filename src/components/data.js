7ca8fad3b33ea125709d6870b1ede872286a67a6
Tutul Kabir <tutulkabir@gmail.com>
2024-02-19 09:16:21 +0530

    project initial setup

---
+++ src/components/data.js
@@ -0,0 +1,26 @@
+const data = [
+  [
+    {
+      id: "1",
+      question: "What is the capital of France?",
+      answer: "The capital of France is Paris.",
+    },
+    {
+      id: "2",
+      question: "What is the largest planet in our solar system?",
+      answer: "The largest planet in our solar system is Jupiter.",
+    },
+    {
+      id: "3",
+      question: "Who wrote the play 'Romeo and Juliet'?",
+      answer: "The play 'Romeo and Juliet' was written by William Shakespeare.",
+    },
+    {
+      id: "4",
+      question: "What is the tallest mountain on Earth?",
+      answer: "The tallest mountain on Earth is Mount Everest.",
+    },
+  ],
+];
+
+export default data;
---
+++ src/components/index.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+//single selection
+//multiple selection
+const Accordion = () => {
+  const [selected, setSelected] = useState(null);
+  return <div className="wrapper"></div>;
+};
+
+export default Accordion;
