0d2c8b62f31e7858a5d5cb53fbc84e35b65b61ba
Tutul Kabir <tutulkabir@gmail.com>
2024-02-19 12:00:22 +0530

    handling multiSelection

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -5,25 +5,49 @@ import "./styles.css";
 //multiple selection
 const Accordion = () => {
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
   const handleSingleSelection = (getCurrentId) => {
     setSelected(getCurrentId === selected ? null : getCurrentId);
   };
 
+  const handleMultiSelection = (getCurrentId) => {
+    let copyMultiple = [...multiple];
+
+    const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
+
+    if (findIndexOfCurrentId === -1) {
+      copyMultiple.push(getCurrentId);
+    } else {
+      copyMultiple.splice(findIndexOfCurrentId, 1);
+    }
+    setMultiple(copyMultiple);
+  };
+
+  console.log(selected, multiple);
   return (
     <div className="wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        Enable Multiple Selection
+      </button>
       <div className="accordion">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div key={dataItem.id} className="item">
               <div
-                onClick={() => handleSingleSelection(dataItem.id)}
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
                 className="title"
               >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
-              {selected === dataItem.id ? (
+              {selected === dataItem.id ||
+              multiple.indexOf(dataItem.id) !== -1 ? (
                 <div className="content">{dataItem.answer}</div>
               ) : null}
             </div>
