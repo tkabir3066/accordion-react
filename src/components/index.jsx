7e6634870576ebacd2497537d278208ca102efee
Tutul Kabir <tutulkabir@gmail.com>
2024-02-19 10:30:41 +0530

    handled single selection part

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,9 +1,39 @@
 import React, { useState } from "react";
+import data from "./data.js";
+import "./styles.css";
 //single selection
 //multiple selection
 const Accordion = () => {
   const [selected, setSelected] = useState(null);
-  return <div className="wrapper"></div>;
+
+  const handleSingleSelection = (getCurrentId) => {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div key={dataItem.id} className="item">
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
+            </div>
+          ))
+        ) : (
+          <div>No data Found!</div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Accordion;
